9943bb7c382f7eb733a2034d92248297d6657335
Ad1tya-007 <adityakul0314@gmail.com>
2022-02-24 16:56:28 -0800

    app
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import MovieRow from "./MovieRow";
+import requests from "./request";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NOT NETFLIX!</h1>
+      <MovieRow
+        title="NOT NETFLIX ORIGINALS"
+        getUrl={requests.getNetflixOriginals}
+      />
+      <MovieRow title="TRENDING NOW" getUrl={requests.getTrending} />
+      <MovieRow title="TOP RATED" getUrl={requests.getTopRated} />
+      <MovieRow title="ACTION MOVIES" getUrl={requests.getActionMovies} />
+      <MovieRow title="COMEDY MOVIES" getUrl={requests.getComedyMovies} />
+      <MovieRow title="HORROR MOVIES" getUrl={requests.getHorrorMovies} />
+      <MovieRow title="ROMANCE MOVIES" getUrl={requests.getRomanceMovies} />
+      <MovieRow title="DOCUMENTRY" getUrl={requests.getDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
