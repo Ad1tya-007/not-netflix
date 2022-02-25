94df5f73b3a584dc78e63052b76539c89ce2b94b
Ad1tya-007 <adityakul0314@gmail.com>
2022-02-25 11:39:45 -0800

    transition added to navbar: scrollY
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
 
 export default function Navbar() {
+  const [show, showHandle] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        showHandle(true);
+      } else showHandle(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav-show"}`}>
       <img
         className="nav-logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
         alt="Netflix Logo"
       />
       <img
-        className="nav_avatar"
+        className="nav-avatar"
         src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
         alt="Netflix Logo"
       />
